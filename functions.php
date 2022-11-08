637a412deca4747b463b79557d435bd9cfa3250f
Maxim Galsterer <42674385+maximgalsterer@users.noreply.github.com>
2022-11-08 16:52:56 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+/* Course Booking System
+================================================================================================================ */
+
+// Show note before availability
+function cbs_single_course_note_before_availability( $course_id, $date ) {
+	$note = cbs_get_note( $course_id, $date );
+
+	if ( !empty( $note ) ) :
+		?>
+		<p><?php _e( 'Note' ); ?>: <strong><?= $note ?></strong></p>
+		<?php
+	endif;
+}
+add_action( 'cbs_single_course_before_availability', 'cbs_single_course_note_before_availability', 10, 2 );
